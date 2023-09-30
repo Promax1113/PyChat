d3a7fd20e04b717d876c955326ca300ab5a4811d
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2023-09-30 15:31:40 +0200

    Add basic Client object

--- server.py
+++ server.py
@@ -1,15 +1,32 @@
 from socket import socket
-import os, time
+import os, json
 from fernet import Fernet
 from multiprocessing import Process
 from server_data import read_csv, save_csv
 from typing import Final
 from configparser import RawConfigParser
 
+class Client:
+    def __init__(self, client: socket, address, username) -> None:
+        self.__username =  username
+        self.__client = client
+        self.__address = address
+        self.__key = None
+        self.__fernet_obj = None
+
+    def login(self):
+        '''Processes the login of the client.'''
+        self.__key = Fernet.generate_key()
+        self.__fernet_obj = Fernet(self.__key)
+        self.__client.sendall(json.dumps({'sec': self.__key.decode()}).encode())
+        user_data = self.__client.recv(BUFSIZE) # Login details in dict.
 local_socket = socket()
+# local_socket.settimeout(5)
 BUFSIZE: Final[int] = 4096
 
+
 def code_to_str(code: int) -> str:
+    '''Converts http code to letters.'''
     if code == 200: return 'Successfully set up server!'
     else: return 'Error!'
 
@@ -26,6 +43,7 @@ def server_setup(ip: str = None, port: int = None):
         return 404
             
 def socket_test(ip: str, port: int):
+    '''Tests sockets before using them.'''
     global local_socket
     local_socket.bind((ip, port))
     local_socket.listen(5)
@@ -37,10 +55,17 @@ def socket_test(ip: str, port: int):
     test_s.sendall(data_to_send)
     test_data = client.recv(BUFSIZE).decode()
     client.close()
+    test_s.close()
     if test_data == '200':
         print(f'\nServing on {ip}:{port}\n')
         return 200
+    else: return 404
     
+def await_client():
+    '''Waits for client to connect.'''
+    while True:
+        client, addr = local_socket.accept()
+        print(f'Connection from {addr} incoming. Accepting...')
 
 def create_child_process(client: object, target, extra_args):
     '''Creates a child process that has a target and returns something.'''
@@ -50,5 +75,4 @@ if __name__ == '__main__':
 
     print(code_to_str(int(server_setup())))
     print('Awaiting connections...')
-    while True:
-        time.sleep(5)
+    await_client()
